691213f66e1d23be234eb7c5c7f7c8d88cc28f43
varun <varun.zenesys@gmail.com>
2025-06-10 17:03:58 +0530

    first commit

---
+++ MyApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyApp
+//
+//  Created by Varun Sharma on 10/06/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, worl!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyAppTests/MyAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyAppTests.swift
+//  MyAppTests
+//
+//  Created by Varun Sharma on 10/06/25.
+//
+
+import Testing
+@testable import MyApp
+
+struct MyAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
